d5e25af9acb25ec2df77773db32546d4c6d2da97
Sandra Cristina Mitsue <106687449+SandraMitsue@users.noreply.github.com>
2022-09-19 19:30:40 -0300

    Adicionando comentários
    Inserindo comentários a fim de documentar o  programa,

--- src/Somando.java
+++ src/Somando.java
@@ -1,9 +1,15 @@
+//Programa de adição, inserindo dois números e depois somando-os
+// utilizando a classe scanner
 import java.util.Scanner;
 
 public class Somando {
+    //método main inicia a execução java
     public static void main(String[] args)
     {
+        // cria um scanner para obter entrada a partir da janela de comando
         Scanner input = new Scanner(System.in);
+        
+        // Adição das variáveis
         int numero1;
         int numero2;
         int soma;
@@ -17,5 +23,6 @@ public class Somando {
         soma = numero1 + numero2;
 
         System.out.printf("A soma é %d%n", soma);
-    }
-}
+        
+    }  //fim do método main
+} //fim da classe Somando
